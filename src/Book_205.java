1fcd30edf33cb1b76853da079c73dc760d7387c0
exam <exam@Bcalab4-m05>
2023-05-04 09:25:47 +0530

    first commit

---
+++ src/Book_205.java
@@ -0,0 +1,19 @@
+
+public class Book_205 {
+
+	int page_num;
+	int total_pages;
+	
+	Book_205(int total_pages)
+	{
+		this.total_pages = total_pages;
+		this.page_num = 1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
